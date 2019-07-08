6d14fa1c225ec0343fb9e65112e5f44eb7039da4
shualton <shualton@gmail.com>
2019-07-08 01:08:59 -0700

    configured firebase

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZ70epQwR1bmkOCrovGX9xQ55CHoN-Qrc",
+    authDomain: "super-original-project.firebaseapp.com",
+    databaseURL: "https://super-original-project.firebaseio.com",
+    projectId: "super-original-project",
+    storageBucket: "super-original-project.appspot.com",
+    messagingSenderId: "442170095409",
+    appId: "1:442170095409:web:4371a828a6f599ab"
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  
